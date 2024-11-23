e560520722c204831b4c23feef056235ce76956b
Evgenii-379 <152536714+Evgenii-379@users.noreply.github.com>
2024-11-23 14:04:10 +0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -2,3 +2,8 @@
 # vm_web_name = "${var.vm_web_name}-${ var.vm_web_cores }-${ var.vm_web_memory }"
 # vm_db_name  = "${var.vm_db_name}-${ var.vm_db_cores }-${ var.vm_db_memory }"
 #}
+
+locals {
+  vm_web_name = "${var.vm_web_name}-${var.vms_resources["web"].cores}-${var.vms_resources["web"].memory}"
+  vm_db_name  = "${var.vm_db_name}-${var.vms_resources["db"].cores}-${var.vms_resources["db"].memory}"
+}
