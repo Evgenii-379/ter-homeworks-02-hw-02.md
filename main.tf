b7ee0c0a30f04146d5e7e05f4725535d1fb82ab2
Evgenii-379 <152536714+Evgenii-379@users.noreply.github.com>
2024-11-23 14:40:19 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "yandex_compute_image" "ubuntu" {
 }
  
 resource "yandex_compute_instance" "platform" {
-#  name        = local.vm_web_name
+  name        = local.vm_web_name
 #  name        = var.vm_web_name
   platform_id = var.vm_web_platform_id
  
@@ -70,7 +70,7 @@ resource "yandex_vpc_subnet" "develop_db" {
 }
  
 resource "yandex_compute_instance" "platform_db" {
-#  name        = local.vm_db_name
+  name        = local.vm_db_name
 #  name        = var.vm_db_name
   platform_id = var.vm_db_platform_id
   zone = "ru-central1-b"
